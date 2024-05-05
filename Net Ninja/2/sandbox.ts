9952c4739e3980eda930ce59b4e99ba12bd06ffb
itzaRinna <dutchismahdick@gmail.com>
2024-05-05 13:55:33 +0100

    new:1,2

---
+++ Net Ninja/2/sandbox.ts
@@ -0,0 +1,11 @@
+const character = 'luigi';
+
+console.log(character);
+
+const inputs = document.querySelectorAll('inputs');
+
+console.log(inputs);
+
+inputs.forEach(input => {
+    console.log(input)
+});
